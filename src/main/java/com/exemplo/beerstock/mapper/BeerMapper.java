14dbb1b0c436b92f68adee8cd7fc9fdab0e86a85
Sandro Janotte <sjanotte@gmail.com>
2020-12-02 13:36:04 -0300

    Created project Beer Stock

---
+++ src/main/java/com/exemplo/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,17 @@
+package com.exemplo.beerstock.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.exemplo.beerstock.dto.BeerDTO;
+import com.exemplo.beerstock.entity.Beer;
+
+@Mapper
+public interface BeerMapper {
+
+	BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+	Beer toModel(BeerDTO beerDTO);
+
+	BeerDTO toDTO(Beer beer);
+}
