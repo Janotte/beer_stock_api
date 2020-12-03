bd7e6ef701472ab778000677f89a6a52967b2e1d
Sandro Janotte <sjanotte@gmail.com>
2020-12-03 00:10:53 -0300

    Created test controller for get by name beer

--- src/test/java/com/exemplo/beerstock/controller/BeerControllerTest.java
+++ src/test/java/com/exemplo/beerstock/controller/BeerControllerTest.java
@@ -16,11 +16,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.exemplo.beerstock.builder.BeerDTOBuilder;
 import com.exemplo.beerstock.dto.BeerDTO;
+import com.exemplo.beerstock.exception.BeerNotFoundException;
 import com.exemplo.beerstock.service.BeerService;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,12 +46,13 @@ public class BeerControllerTest {
 	void setUp() {
 		mockMvc = MockMvcBuilders.standaloneSetup(beerController)
 				.setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
-				.setViewResolvers((s, locale) -> new MappingJackson2JsonView()).build();
+				.setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+				.build();
 	}
 
 	@Test
 	void whenPOSTIsCalledThenABeerIsCreated() throws Exception {
-		
+
 		// given
 		BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
 
@@ -68,7 +71,7 @@ public class BeerControllerTest {
 
 	@Test
 	void whenPOSTIsCalledWithoutRequiredFieldThenAnErrorIsReturned() throws Exception {
-		
+
 		// given
 		BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
 		beerDTO.setBrand(null);
@@ -78,6 +81,37 @@ public class BeerControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(asJsonString(beerDTO)))
 				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void whenGETIsCalledWithValidNameThenOkStatusIsReturned() throws Exception {
+		// given
+		BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+		// when
+		when(beerService.findByName(beerDTO.getName())).thenReturn(beerDTO);
+
+		// then
+		mockMvc.perform(MockMvcRequestBuilders.get(BEER_API_URL_PATH + "/" + beerDTO.getName())
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name", is(beerDTO.getName())))
+				.andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+				.andExpect(jsonPath("$.type", is(beerDTO.getType().toString())));
+	}
+
+	@Test
+	void whenGETIsCalledWithoutRegisteredNameThenNotFoundStatusIsReturned() throws Exception {
+		// given
+		BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+		// when
+		when(beerService.findByName(beerDTO.getName())).thenThrow(BeerNotFoundException.class);
+
+		// then
+		mockMvc.perform(MockMvcRequestBuilders.get(BEER_API_URL_PATH + "/" + beerDTO.getName())
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
 	}
 
 }
